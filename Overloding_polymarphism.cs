0709292b32087eab0d76b11923deeaec5a38f489
Pravita Dawkar <pravitadawkar@gmail.com>
2022-02-24 23:27:44 +0530

    polymarphism

---
+++ Overloding_polymarphism.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymarphism
+    // derived class
+{   class Book
+    {
+        //read method 
+        public int read(int a,int b)
+        {
+            return a + b;
+        }
+        //read method
+        public int read(int a, int b,int c)
+        {
+            return a * b *c;
+        }
+        //read method
+        public int read(int a, int b ,int c,int d)
+        {
+            return (a + b)*(c+d);
+        }
+    }
+    //main class
+    class Overloding_polymarphism
+
+    {
+        static void Main(string[] args)
+        {
+            //create nbook new object
+            Book nbook = new Book();
+            Console.WriteLine("read method with 2 para: "+nbook.read(12, 65));
+            Console.WriteLine("read method with 3 para: " + nbook.read(12, 1,5));
+            Console.WriteLine("read method with 4 para: " + nbook.read(12, 5,22,8));
+
+
+
+        }
+    }
+}
---
+++ Overriding_polymarphism.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymarphism
+{
+    //base class
+    class Animal
+    {
+        //method virtual
+        public virtual void eating()
+        {
+            Console.WriteLine("animal is eating");
+        }
+    }
+    //derived class 
+    class Cow:Animal
+    {
+        //override method
+        public override void eating()
+        {
+            Console.WriteLine("cow is eating");
+        }
+    }
+    class Overriding_polymarphism
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+            a.eating();
+            Cow c = new Cow();
+            c.eating();
+        }
+    }
+}
