da96fa6ca05635cdf0d4336e516c49ea4af79cbe
Pravita Dawkar <pravitadawkar@gmail.com>
2022-02-28 11:38:13 +0530

    abstractclass

---
+++ Abstractclass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymarphism
+{
+   abstract class Abstractclass
+    {
+       public abstract void newmethod();
+
+    }
+    class Newclass:Abstractclass
+    {
+         public  override void newmethod()
+        {
+             Console.WriteLine("hi this is method using abstract class");
+        }
+    }
+    class Mainclass
+    { 
+      public static void Main(string[] args)
+        {
+            Newclass obj = new Newclass();
+            obj.newmethod();
+        }
+    }
+}
