d2e204a330c22c2c82cb9d55bd4d187f23a20df9
Pravita Dawkar <pravitadawkar@gmail.com>
2022-02-28 11:12:32 +0530

    interface

---
+++ Interface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymarphism
+{
+    interface IFirstInterface
+    {
+        void myMethod(); // interface method
+    }
+
+
+    // Implement multiple interfaces
+    class DemoClass : IFirstInterface
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("Some text..");
+        }
+       
+    }
+
+    class Myinterface
+    {
+        static void Main(string[] args)
+        {
+            DemoClass myObj = new DemoClass();
+            myObj.myMethod();
+            
+        }
+    }
+}
+
